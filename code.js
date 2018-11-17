a34b5fda5a0238f66538fed65aa83fba4bf39fcf
Unknown <florian.borie@ynov.com>
2018-11-17 13:00:51 +0100

    code

--- code.js
+++ code.js
@@ -2,7 +2,7 @@
 
 module .exports = { 
   consumer_key : 'V5qldfwD0grCPXqJtM2PhBuuq', 
-  consumer_secret : '541szJxPM0w9ygQgTTJ4txWljikMEsxHySPIsRLazLDLGog08h', 
+  consumer_secret : '541szJxPM0w9ygQgTTJ4txWljikMEsxHySPIsRLazLDLGog08h' , 
   access_token_key: '999413403075661824-a7hzYGZltXf0HBIIRAcWZm6nnLFlqKS',
   access_token_secret: 'WiJQ95XRApH2jvx8HhUGRpZAi0lkwVCfr2xc3lqAOSiFf'
-}
+};
