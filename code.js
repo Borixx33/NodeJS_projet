875a18c31c5d1971da856ccafa89df043be1aa9f
Unknown <florian.borie@ynov.com>
2018-11-26 12:23:21 +0100

    code supprimer

--- code.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-// Identifiant et token pour ce connecter à Twitter
-module .exports = { 
-  consumer_key : 'V5qldfwD0grCPXqJtM2PhBuuq', 
-  consumer_secret : '541szJxPM0w9ygQgTTJ4txWljikMEsxHySPIsRLazLDLGog08h' , 
-  access_token_key: '999413403075661824-a7hzYGZltXf0HBIIRAcWZm6nnLFlqKS',
-  access_token_secret: 'WiJQ95XRApH2jvx8HhUGRpZAi0lkwVCfr2xc3lqAOSiFf'
-};
